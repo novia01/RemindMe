ade12a0f5166e1ca28ad9c7f6e7db20479b16c29
novia01 <74140777+novia01@users.noreply.github.com>
2022-12-17 01:17:19 +0700

    modified:   lib/users/fragmen/dashboard.dart

--- lib/users/fragmen/dashboard.dart
+++ lib/users/fragmen/dashboard.dart
@@ -1,6 +1,7 @@
 import 'package:farmasi_yakkum/users/authentication/login.dart';
 import 'package:farmasi_yakkum/users/fragmen/detail_list.dart';
 import 'package:farmasi_yakkum/users/fragmen/list_obat.dart';
+import 'package:farmasi_yakkum/users/modal/user.dart';
 import 'package:farmasi_yakkum/users/userPreferences/current_user.dart';
 import 'package:farmasi_yakkum/users/userPreferences/user_preferences.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
@@ -16,11 +17,23 @@ class Dashboard extends StatefulWidget {
 
 class _DashboardState extends State<Dashboard> {
   
-  CurrentUser _rememberCurrentUser = Get.put(CurrentUser());
-  final CurrentUser _currentUser = Get.put(CurrentUser());
-  late FirebaseMessaging messaging;
+  FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance; // Change here
+  CurrentUser _currentUser = CurrentUser();
+  var userName = null;
 
-  signOutUser () async {
+  Future<String?> getCurrentUserID() async {
+    User? user = await RememberUserPrefs.readUserInfo(); 
+    _currentUser.setCurrentUser(user);
+
+    setState(() {
+      userName = user?.namaPasien;
+    });
+
+    print(_currentUser.user.namaPasien);
+    return user?.id_pasien;
+  }
+
+  signOutUser() async {
     var resultResponse = await Get.dialog(
       AlertDialog (
         backgroundColor: Colors.blueGrey,
@@ -61,31 +74,40 @@ class _DashboardState extends State<Dashboard> {
       ),
     );
     if (resultResponse == "LoggedOut") {
-      RememberUserPrefs.removeUserInfo () 
-      .then((value)
-      {
+      getCurrentUserID().then((userID) {
+        // print("Logout");
+        // print(userID);
+        var topic = "Reminder${userID}";
+        _firebaseMessaging.unsubscribeFromTopic(topic);
+      });
+      await RememberUserPrefs.removeUserInfo().then((value) {
         Get.off(Login());
       });
     }
   }
   
-  @override
   void initState()  {
     super.initState();
-    FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance; // Change here
-    _firebaseMessaging.subscribeToTopic("Reminder${_currentUser.user.id_pasien}");
 
-    print(_currentUser.user.id_pasien);
+    getCurrentUserID().then((userID) {
+      // print("getCurrentUserID");
+      // print(userID);
+
+      var topic = "Reminder${userID}";
+      _firebaseMessaging.subscribeToTopic(topic);
+      // print(topic);
+    });
 
   // Handling Message In Foreground
     FirebaseMessaging.onMessage.listen((RemoteMessage event) {
-      print("message recieved");
+      // print("message recieved");
+      // print(_currentUser.user.namaPasien);
+      // print(userName);
       showDialog(
         context: context,
         builder: (BuildContext context) {
           return AlertDialog(
             title: Text(event.data["title"]),
-            // content: Text(event.notification!.body!),
             content: SingleChildScrollView(
               child: ListBody(
                 children: <Widget>[
@@ -111,7 +133,7 @@ class _DashboardState extends State<Dashboard> {
                 child: const Text("Lihat Detail"),
                 onPressed: () {
                   Navigator.pushReplacement(
-                    context, MaterialPageRoute(builder: (_) => DetailObat())
+                    context, MaterialPageRoute(builder: (_) => ListObat())
                   );
                 },
               ),
@@ -124,163 +146,157 @@ class _DashboardState extends State<Dashboard> {
     FirebaseMessaging.onMessageOpenedApp.listen((message) {
       print('Message cliked');
       Navigator.push(
-        context, MaterialPageRoute(builder: (BuildContext context) => DetailObat())
+        context, MaterialPageRoute(builder: (BuildContext context) => ListObat())
       );
     }); 
   }
-  
+
+  @override
   Widget build(BuildContext context) {
-    return GetBuilder(
-      init: CurrentUser(),
-      initState: (currrentState){
-        _rememberCurrentUser.getUserInfo();
-      },
-      builder: (controller) {
-        return Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.white,
-            title: Text('Hai, Admin Example hehe',
-            style: TextStyle(fontSize: 18, color: Colors.grey.shade700)),
-            leading : GestureDetector (
-              onTap: () {},
-                child: const Icon(
-                  Icons.account_circle_rounded,
-                  color: Colors.blueGrey,
-                ),
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        title: Text(
+          'Hai ${userName}', 
+          style: TextStyle(fontSize: 18, color: Colors.grey.shade700)),
+          leading : GestureDetector (
+            onTap: () {},
+            child: const Icon(
+              Icons.account_circle_rounded,
+              color: Colors.blueGrey,
             ),
+        ),
 
-            actions: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(right: 20.0),
-                child: GestureDetector(
-                  child: const Icon(
-                    Icons.notifications,
-                    color: Colors.blueGrey,
-                  ),
-                )
+        actions: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(right: 20.0),
+            child: GestureDetector(
+              child: const Icon(
+                Icons.notifications,
+                color: Colors.blueGrey,
               ),
-            ],
+            )
           ),
+        ],
+      ),
 
-          body: 
-          SingleChildScrollView (
-            child: Column(
-              children: [
-                Center(  
-                  child: Container(
-                    height: 140,
-                    width: 340,
-                    decoration: const BoxDecoration(
-                      borderRadius: BorderRadius.only(
-                        bottomRight: Radius.circular(15),
-                        bottomLeft: Radius.circular(15),
-                        topRight: Radius.circular(15),
-                        topLeft: Radius.circular(15),
-                      ),
-                      color: Colors.blue,
-                    ),
-                    margin: const EdgeInsets.only(bottom: 100),
-                    child: Stack (
-                      children: [
-                        Positioned(
-                          top: 0,
-                          right: 0,
-                          child: Container(
-                            height: 140,
-                            width: 130,
-                            decoration: BoxDecoration(
-                              color: Colors.indigo.shade900,
-                              borderRadius: const BorderRadius.only(
-                                topRight: Radius.circular(15),
-                                bottomRight: Radius.circular(15),
-                                topLeft: Radius.circular(0),
-                                bottomLeft: Radius.circular(170),
-                              ),
-                            ),
-                          ),
-                        ),
-                        Positioned(
-                          right: 10,
-                          top: 20,
-                          child: Row(
-                            children: <Widget>[
-                              Image.asset('assets/images/yakkum-logo-white.png'),
-                            ],
-                          ),
-                        ),
-                        Positioned(
-                          top: 10,
-                          left: 5,
-                          child: Column(
-                            children: const [
-                              Text('Yayasan Kristen \n untuk \n Kesehatan Umum\n',
-                                style: TextStyle(fontSize: 24, color: Colors.white)),
-                              Text('Berkarya Berdasarkan Kasih',
-                                style: TextStyle(fontSize: 20, fontStyle: FontStyle.italic, color: Colors.white)
-                              ),  
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
+      body: 
+      SingleChildScrollView (
+        child: Column(
+          children: [
+            Center(  
+              child: Container(
+                height: 140,
+                width: 340,
+                decoration: const BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(15),
+                    bottomLeft: Radius.circular(15),
+                    topRight: Radius.circular(15),
+                    topLeft: Radius.circular(15),
                   ),
+                  color: Colors.blue,
                 ),
-
-                Column(
+                margin: const EdgeInsets.only(bottom: 100),
+                child: Stack (
                   children: [
-                    Container( 
-                      child: TextButton(
-                        onPressed: () {
-                          Get.to(ListObat());
-                        },
-                        child : Material(
-                          color: Colors.white,
-                          elevation: 10,
-                          borderRadius: BorderRadius.circular(20),
-                          clipBehavior: Clip.antiAliasWithSaveLayer,
-                          child: Column(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              Ink.image(
-                                image: const AssetImage('assets/images/obat.png'), 
-                                height: 120,
-                                width: 120,
-                                fit: BoxFit.cover,
-                              ),
-                              const SizedBox(height: 6,),
-                              const Text('Konsumsi Obat',
-                                style: TextStyle(fontSize: 16, color: Colors.blue, fontWeight: FontWeight.bold),
-                              ),
-                              const SizedBox(height: 8,),
-                            ],
+                    Positioned(
+                      top: 0,
+                      right: 0,
+                      child: Container(
+                        height: 140,
+                        width: 130,
+                        decoration: BoxDecoration(
+                          color: Colors.indigo.shade900,
+                          borderRadius: const BorderRadius.only(
+                            topRight: Radius.circular(15),
+                            bottomRight: Radius.circular(15),
+                            topLeft: Radius.circular(0),
+                            bottomLeft: Radius.circular(170),
                           ),
                         ),
+                      ),
+                    ),
+                    Positioned(
+                      right: 10,
+                      top: 20,
+                      child: Row(
+                        children: <Widget>[
+                          Image.asset('assets/images/yakkum-logo-white.png'),
+                        ],
+                      ),
+                    ),
+                    Positioned(
+                      top: 10,
+                      left: 5,
+                      child: Column(
+                        children: const [
+                          Text('Yayasan Kristen \n untuk \n Kesehatan Umum\n',
+                            style: TextStyle(fontSize: 24, color: Colors.white)),
+                          Text('Berkarya Berdasarkan Kasih',
+                            style: TextStyle(fontSize: 20, fontStyle: FontStyle.italic, color: Colors.white)
+                          ),  
+                        ],
                       ),
                     ),
                   ],
                 ),
-                Container(
-                  height: 40,
-                  width: 340,
-                  decoration: BoxDecoration(
-                      color: Colors.blue, borderRadius: BorderRadius.circular(20)),
-                  margin: EdgeInsets.only(top: 150),
-                  child: InkWell(
-                    onTap: () {
-                      signOutUser();
+              ),
+            ),
+
+            Column(
+              children: [
+                Container( 
+                  child: TextButton(
+                    onPressed: () {
+                      Get.to(ListObat());
                     },
-                    child: const Text(
-                      'Logout',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+                    child : Material(
+                      color: Colors.white,
+                      elevation: 10,
+                      borderRadius: BorderRadius.circular(20),
+                      clipBehavior: Clip.antiAliasWithSaveLayer,
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Ink.image(
+                            image: const AssetImage('assets/images/obat.png'), 
+                            height: 120,
+                            width: 120,
+                            fit: BoxFit.cover,
+                          ),
+                          const SizedBox(height: 6,),
+                          const Text('Konsumsi Obat',
+                            style: TextStyle(fontSize: 16, color: Colors.blue, fontWeight: FontWeight.bold),
+                          ),
+                          const SizedBox(height: 8,),
+                        ],
+                      ),
                     ),
                   ),
                 ),
               ],
             ),
-          ),
-        );
-      }
-    ); 
+            Container(
+              height: 40,
+              width: 340,
+              decoration: BoxDecoration(
+                  color: Colors.blue, borderRadius: BorderRadius.circular(20)),
+              margin: EdgeInsets.only(top: 150),
+              child: InkWell(
+                onTap: () {
+                  signOutUser();
+                },
+                child: const Text(
+                  'Logout',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.white, fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
